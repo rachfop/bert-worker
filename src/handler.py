aceecb2a59094f0b1d949909b0d1b08f8dcfa1a0
Patrick <prachford@icloud.com>
2024-07-08 11:10:29 -0700

    black format on handler

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,5 @@
-import torch
 import runpod
+import torch
 from runpod.serverless.utils.rp_validator import validate
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
@@ -7,23 +7,22 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print(device)
 INPUT_SCHEMA = {
-    'sequence': {
-        'type': str,
-        'required': True
+    "sequence": {"type": str, "required": True},
+    "labels": {
+        "type": list,
+        "required": True,
     },
-    'labels': {
-        'type': list,
-        'required': True,
-    }
 }
+
 
 def classify_text(sequence, labels):
     model = AutoModelForSequenceClassification.from_pretrained(
         "facebook/bart-large-mnli",
-        local_files_only=False  # Change this to False to download if not available locally
+        local_files_only=False,  # Change this to False to download if not available locally
     ).to(device)
     tokenizer = AutoTokenizer.from_pretrained(
-        "facebook/bart-large-mnli", local_files_only=False)  # Change this to False to download if not available locally
+        "facebook/bart-large-mnli", local_files_only=False
+    )  # Change this to False to download if not available locally
 
     classifier = pipeline(
         "zero-shot-classification",
@@ -34,17 +33,16 @@ def classify_text(sequence, labels):
 
     return classifier(sequence, labels, multi_label=True)
 
+
 async def handler(job):
-    val_input = validate(job['input'], INPUT_SCHEMA)
-    if 'errors' in val_input:
-        return {"error": val_input['errors']}
-    val_input = val_input['validated_input']
+    val_input = validate(job["input"], INPUT_SCHEMA)
+    if "errors" in val_input:
+        return {"error": val_input["errors"]}
+    val_input = val_input["validated_input"]
 
     classification_result = classify_text(val_input["sequence"], val_input["labels"])
-    
-    return {
-        "classification_result": classification_result,
-        "device": str(device)
-    }
+
+    return {"classification_result": classification_result, "device": str(device)}
+
 
 runpod.serverless.start({"handler": handler, "concurrency_modifier": lambda x: 1000})
